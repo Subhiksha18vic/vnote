38dbbd2a0b510e5db3b2cc474c3f0fc0c49441cd
veroni-d20 <67852686+veroni-d20@users.noreply.github.com>
2021-05-11 23:40:42 +0530

    Fixed errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,7 @@ import Layout from "./Layout";
 
 import CreateNote from "./components/home/CreateNote";
 import GetNotes from "./components/home/GetNotes";
-import Dark from "./Dark";
 
-//import Darktheme from "./components/home/Darktheme";
 
 export default function Home() {
   const easing = [0.6, -0.05, 0.01, 0.99];
@@ -36,7 +34,7 @@ export default function Home() {
   return (
     
       <Layout>
-        <Dark/>
+       
         <motion.div exit="exit" initial="initial" animate="animate">
           <motion.div variants={fadeIn1}>
             <CreateNote></CreateNote>
